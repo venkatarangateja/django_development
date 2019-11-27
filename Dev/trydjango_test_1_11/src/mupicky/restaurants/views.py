13fc728e703a07c88695704a5d1f1d70db7a6d4a
venkatarangateja <venkatarangateja@gmail.com>
2019-11-27 16:46:27 +0530

    modified settings

--- Dev/trydjango_test_1_11/src/mupicky/restaurants/views.py
+++ Dev/trydjango_test_1_11/src/mupicky/restaurants/views.py
@@ -2,8 +2,11 @@
 from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
-
+import random
 
 def home(request):
+	some_list = list()
+	for numbers in range(1,100):
+		some_list.append(numbers)
 	#return HttpResponse("hello")
-	return render(request, "base.html", {'html_var': 'teja'})
+	return render(request, "base.html", {'bool_item': True, "num": random.randint(0,1000), "some_list": some_list})
