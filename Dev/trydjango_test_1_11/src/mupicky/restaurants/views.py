9434b8be730f7b65987d4fd91c747f4916d421c0
venkatarangateja <venkatarangateja@gmail.com>
2019-11-27 17:28:24 +0530

    my own html and views code

--- Dev/trydjango_test_1_11/src/mupicky/restaurants/views.py
+++ Dev/trydjango_test_1_11/src/mupicky/restaurants/views.py
@@ -5,8 +5,27 @@ from django.shortcuts import render
 import random
 
 def home(request):
-	some_list = list()
-	for numbers in range(1,100):
-		some_list.append(numbers)
-	#return HttpResponse("hello")
-	return render(request, "base.html", {'bool_item': True, "num": random.randint(0,1000), "some_list": some_list})
+	Even_list = list()
+	odd_list = list()
+	condition_statement = 'is not enabled'
+	if condition_statement == 'is enabled':
+		for numbers in range(1,random.randint(0,10000)):
+			if numbers%2 == 0:
+				Even_list.append(numbers)
+		#return HttpResponse("hello")
+		context = {
+				"Even_list": Even_list,
+				"bool_item": True,
+				"condition_statement": "is enabled"
+				}
+		return render(request, "base.html", context)
+	elif condition_statement == 'is not enabled':
+		for num in range(0,1000):
+			if num%2 == 1:
+				odd_list.append(num)
+		context = {
+				"odd_list": odd_list,
+				"bool_item": True,
+				"condition_statement": "is not enabled"
+				}
+		return render(request,"base.html",context)
