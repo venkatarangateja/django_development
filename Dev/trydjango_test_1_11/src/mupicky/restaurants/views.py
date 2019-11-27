7fc934ebe5658f5ebca568e5db338c090e4daa5a
venkatarangateja <venkatarangateja@gmail.com>
2019-11-27 18:58:04 +0530

    my own html and views code

--- Dev/trydjango_test_1_11/src/mupicky/restaurants/views.py
+++ Dev/trydjango_test_1_11/src/mupicky/restaurants/views.py
@@ -12,13 +12,13 @@ def home(request):
 		for numbers in range(1,random.randint(0,10000)):
 			if numbers%2 == 0:
 				Even_list.append(numbers)
-		#return HttpResponse("hello")
 		context = {
 				"Even_list": Even_list,
 				"bool_item": True,
 				"condition_statement": "is enabled"
 				}
-		return render(request, "base.html", context)
+		return render(request, "home.html", context)
+
 	elif condition_statement == 'is not enabled':
 		for num in range(0,1000):
 			if num%2 == 1:
@@ -28,4 +28,10 @@ def home(request):
 				"bool_item": True,
 				"condition_statement": "is not enabled"
 				}
-		return render(request,"base.html",context)
+		return render(request,"home.html",context)
+
+def about(request):
+	return render(request,"about.html",context={})
+
+def contact(request):
+	return render(request,"contact.html",context={})
