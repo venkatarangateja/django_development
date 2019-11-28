d3954f73d0c050017eccf882f4e003989896502f
venkatarangateja <venkatarangateja@gmail.com>
2019-11-28 17:50:12 +0530

    updated_templates and models files

--- Dev/trydjango_test_1_11/src/mupicky/mupicky/urls.py
+++ Dev/trydjango_test_1_11/src/mupicky/mupicky/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from restaurants.views import HomeView
+from restaurants.views import Restaurant_list_view
 from django.views.generic import TemplateView
-
 urlpatterns = [
 	url(r'^admin/', admin.site.urls),
-	url(r'^$', HomeView.as_view()),
+	url(r'^$', TemplateView.as_view(template_name = "home.html")),
+	url(r'^restaurants/$', Restaurant_list_view),
 	url(r'^about/$', TemplateView.as_view(template_name = "about.html")),
 	url(r'^contact/', TemplateView.as_view(template_name = "contact.html"))
 ]
--- Dev/trydjango_test_1_11/src/mupicky/restaurants/views.py
+++ Dev/trydjango_test_1_11/src/mupicky/restaurants/views.py
@@ -4,7 +4,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
-import random
 
 # def home(request):
 # 	Even_list = list()
@@ -38,31 +37,7 @@ import random
 # def contact(request):
 # 	return render(request,"contact.html",context={})
 
-class HomeView(TemplateView):
-	template_name = "home.html"
-	def get_context_data(self, *args, **kwargs):
-		context = super(HomeView,self).get_context_data(*args,**kwargs)
-		Even_list = list()
-		odd_list = list()
-		condition_statement = 'is not enabled'
-		if condition_statement == 'is enabled':
-			for numbers in range(1, random.randint(0, 10000)):
-				if numbers % 2 == 0:
-					Even_list.append(numbers)
-			context = {
-					"Even_list": Even_list,
-					"bool_item": True,
-					"condition_statement": "is enabled"
-					}
-			return context
-
-		elif condition_statement == 'is not enabled':
-			for num in range(0, 1000):
-				if num % 2 == 1:
-					odd_list.append(num)
-			context = {
-					"odd_list": odd_list,
-					"bool_item": True,
-					"condition_statement": "is not enabled"
-					}
-			return context
+def Restaurant_list_view(request):
+	template_name= 'restaurants/restaurant_list.html'
+	context = {"obj_list": [12,2,323,23]}
+	return render(request,template_name,context)
